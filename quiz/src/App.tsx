382f4826de48231e04bc9ea3a29bee3790d3c17f
FayeCarter <fayecarter333@gmail.com>
2020-08-04 14:12:22 +0100

    Create App scaffold

--- quiz/src/App.tsx
+++ quiz/src/App.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+//Components
+import QuestionCard from './components/QuestionCard';
 
-function App() {
+function App = () => {
+
+  const startTrivia = async () => {
+
+  };
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    
+  };
+
+  const nextQuestion = () => {
+
+  };
+
   return (
-    <div className="App">Quiz</div>
-  );
+    <div className="App">
+      <h1>Quiz</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score: </p>
+      <p>Loading Qyesions ...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+    );
 }
 
 export default App;
