5c88f41fdf61ad1cccc4f66cb0c97c259f1fcbb6
FayeCarter <fayecarter333@gmail.com>
2020-08-04 14:58:34 +0100

    Creat Answer type

--- quiz/src/App.tsx
+++ quiz/src/App.tsx
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
 //Components
 import QuestionCard from './components/QuestionCard';
-import { fetchQuizQuestions, Difficulty } from './API';
+import { fetchQuizQuestions, Difficulty, QuestionState } from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+
+const TOTAL_QUESTIONS = 10;
 
 const App = () => {
 
-  const TOTAL_QUESTIONS = 10;
-
   const [loading, setLoading] = useState(false);
-  const [ questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] =useState([]);
+  const [userAnswers, setUserAnswers] =useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
